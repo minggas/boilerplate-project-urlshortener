e00680423fc760cb90b3f13bae8f3f62c663ad21
Fabricio Asfora Lira <32206134+minggas@users.noreply.github.com>
2018-08-11 02:43:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,14 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var dns = require('dns');
+var validUrl = require('valid-url');
 
 var cors = require('cors');
 
 var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
@@ -18,6 +22,8 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -30,6 +36,43 @@ app.get('/', function(req, res){
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+var Url = require('./Models.js').UrlModel;
+var findUrl = require('./Models.js').findUrl;
+var saveUrl = require('./Models.js').saveUrl;
+
+// Save shortened URL in DB
+router.post('/shorturl/new', function(req, res, next){
+  var url = req.body.url;
+   dns.lookup(url.replace("https://", "").replace("http://", ""),(err, address, family) => {
+     if(address){
+       Url.findOne({long_end: req.body.url}, function(err, data) {
+         if(err) { return (next(err)); }
+         if(!data){
+           saveUrl(url, function(err, data){
+              if(err) { return (next(err)); }
+              res.json({original_url:data.long_end, short_url: data.id});
+           });
+         } else {
+           res.json({original_url:data.long_end, short_url: data.id});
+         }
+      });
+     }else{
+    res.json({"error":"invalid URL"});
+    }  
+   });  
+});
+
+
+router.get('/shorturl/:id', function(req, res, next){  
+  Url.findById(req.params.id, function(err, pers) {
+       if(err) { return (next(err)); }
+       return res.redirect(pers.long_end); 
+     });
+});
+
+
+app.use('/api', router);
 
 
 app.listen(port, function () {
